dfd12818cdc1c047f7457f94ade0e27c422a1223
BassT <sebastian.richter@student.kit.edu>
2015-03-23 16:53:23 +0100

    added flag for disabling working area separation

--- vendor/assets/javascripts/fullcalendar.js
+++ vendor/assets/javascripts/fullcalendar.js
@@ -4929,11 +4929,13 @@ DayGrid.mixin({
 
 		// concatenate levels by working area and add empty row
 		for (i = 0; i < workingAreas.length; i++) {
-			// add padding to each element of last level of each working area
-			var lastLevelOfWorkingArea = levelsByWorkingArea[workingAreas[i]][levelsByWorkingArea[workingAreas[i]].length - 1];
-			for (k = 0; k < lastLevelOfWorkingArea.length; k++) {
-				lastLevelOfWorkingArea[k].paddingBottom = "45px";
-			}
+			// add padding to each element of last level of each working area - if desired
+            if (getCookie('areaSort') !== "false") {
+                var lastLevelOfWorkingArea = levelsByWorkingArea[workingAreas[i]][levelsByWorkingArea[workingAreas[i]].length - 1];
+                for (k = 0; k < lastLevelOfWorkingArea.length; k++) {
+                    lastLevelOfWorkingArea[k].paddingBottom = "45px";
+                }
+            }
 			levels = levels.concat(levelsByWorkingArea[workingAreas[i]]);
 		}
 
